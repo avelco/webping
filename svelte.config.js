8241c5b401ca202275dad83972e7d5887b18260c
Alejandro Velasco <avelasco@conyko.com>
2025-04-26 22:49:42 -0500

    wrangler agregado

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,21 @@
 import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+  preprocess: vitePreprocess(),
+
+  kit: {
+    // Use the Cloudflare adapter
+    adapter: adapter({
+      // See adapter options below
+      routes: {
+        include: ['/*'], // Include all routes by default
+        exclude: ['<all>'] // Exclude default Cloudflare asset handling if needed
+      }
+    })
+    // You might have other kit options here
+  }
 };
 
 export default config;
